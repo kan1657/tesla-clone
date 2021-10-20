d5fe15f9a991da9c04956d0f8247927a149ffa9e
Kan <kan1657@hotmail.com>
2021-10-21 00:50:45 +0700

    add react reveal to the project

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@
 import React from "react";
 import styled from "styled-components";
 import MenuIcon from "@mui/icons-material/Menu";
+
 function Header() {
   return (
     <Container>
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,19 +1,25 @@
 import React from "react";
 import styled from "styled-components";
+import Fade from "react-reveal/Fade";
+
 function Section(props) {
   const { title, description, backgroundImg, leftBtnText, rightBtnText } =
     props;
   return (
     <Wrap bgImage={backgroundImg}>
-      <ItemText>
-        <h1>{title} </h1>
-        <p>{description}</p>
-      </ItemText>
+      <Fade bottom>
+        <ItemText>
+          <h1>{title} </h1>
+          <p>{description}</p>
+        </ItemText>
+      </Fade>
       <Buttons>
-        <ButtonGroup>
-          <LeftButton>{leftBtnText}</LeftButton>
-          {rightBtnText && <RightButton>{rightBtnText}</RightButton>}
-        </ButtonGroup>
+        <Fade bottom>
+          <ButtonGroup>
+            <LeftButton>{leftBtnText}</LeftButton>
+            {rightBtnText && <RightButton>{rightBtnText}</RightButton>}
+          </ButtonGroup>
+        </Fade>
 
         <DownArrow src="images/down-arrow.svg" />
       </Buttons>
